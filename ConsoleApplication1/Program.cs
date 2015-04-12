4556bd330dfb3e2867f5a1c6598485c64ee0b519
ramon <rdreme@hotmail.com>
2015-04-12 13:28:36 +0200

    Primera subida

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pulse para empezar.");
+            Console.ReadKey();
+            ServiceReference1.Service1Client cliente = new ServiceReference1.Service1Client();
+            for (int i=0;i<100;i++)
+            {
+                Console.WriteLine("Llamada a servicio: " + i.ToString() + " : " + DateTime.Now.ToString());
+                cliente.GetData(i);
+                Console.WriteLine("Llegada del servicio: " + i.ToString() + " : " + DateTime.Now.ToString());
+            }
+            Console.WriteLine("Pulse para finalizar.");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WcfService1OneWay/Service1.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+
+namespace WcfService1OneWay
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.PerCall)]
+    public class Service1 : IService1
+    {
+
+        public void GetData(int value)
+        {
+            Thread.Sleep(2000);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
